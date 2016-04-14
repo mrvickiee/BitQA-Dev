513b00d59589642f4478a386b3f584e8a6c0f7b8
Asjad <asjadathick@gmail.com>
2016-04-14 10:56:48 +1000

    Initialised empty Xcode project

---
+++ Backend-code/Xcode/BitQA-Backend/BitQA-Backend/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BitQA-Backend
+//
+//  Created by Asjad Athick on 14/04/2016.
+//  Copyright © 2016 Asjad Athick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
