143b1a6d4111022a189a5a977317baec0514087e
James Marino <jamesmarinoau@gmail.com>
2016-04-29 23:20:20 +1000

    Web hook merged as single command

--- api/webhooks/commit/index.php
+++ api/webhooks/commit/index.php
@@ -1,13 +1,10 @@
 <?php
 	
-	$commands = array(
-			"cd /var/www/csci222.com/ 2>&1",
-			"git pull git@github.com:asjadathick/BitQA-Dev.git 2>&1",
-			"cd /var/www/csci222.com/BitQA 2>&1",
-			"sudo make build 2>&1"
-		);
+	$commands = 
+			"cd /var/www/csci222.com/ 2>&1; " .
+			"git pull git@github.com:asjadathick/BitQA-Dev.git 2>&1; " .
+			"cd /var/www/csci222.com/BitQA 2>&1; " .
+			"make build 2>&1; ";
 
-	foreach ($commands as $command) {
-		$output = shell_exec($command);
-		echo "<p>$ {$command} <br>" . $output . "<br></p>";
-	};
+	$output = shell_exec($command);
+	echo "<p>$ {$command} <br>" . $output . "<br></p>";
