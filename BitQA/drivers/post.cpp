134307953d1a5e347029f7b4ce9e649c715de680
James Marino <jamesmarinoau@gmail.com>
2016-05-02 00:12:22 +1000

    Added Posting Question Page

---
+++ BitQA/drivers/post.cpp
@@ -0,0 +1,15 @@
+#include "../process/post.hpp"
+#include "../includes/includes.hpp"
+
+using namespace std;
+
+int main()
+{
+	Includes::displayHeader();
+	
+	cout << "<h1>Post</h1>";
+	
+	Includes::displayFooter();
+	
+	return 0;
+}
