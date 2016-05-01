15b72e7d2552680381b0eb8c0cf88de25354b6ed
James Marino <jamesmarinoau@gmail.com>
2016-05-01 23:37:42 +1000

    Web hook now working

--- api/webhooks/commit/index.php
+++ api/webhooks/commit/index.php
@@ -1,10 +1,11 @@
 <?php
 	
-	$commands = 
-			"cd /var/www/csci222.com/ 2>&1; " .
-			"git pull git@github.com:asjadathick/BitQA-Dev.git 2>&1; " .
-			"cd /var/www/csci222.com/BitQA 2>&1; " .
-			"make build 2>&1; ";
+	$commands = array(
+			"sudo -Hu www-data git -C /var/www/csci222.com/ pull git@github.com:asjadathick/BitQA-Dev.git 2>&1",
+			"make -C /var/www/csci222.com/BitQA build 2>&1 "
+		);
 
-	$output = shell_exec($command);
-	echo "<p>$ {$command} <br>" . $output . "<br></p>";
+	foreach ($commands as $command) {
+		$output = shell_exec($command);
+		echo "<p>$ {$command} <br> {$output}<br></p>";
+	};
