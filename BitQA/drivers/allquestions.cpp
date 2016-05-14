e7cc04d8825998436bdab929c075f5056345cea6
Asjad <asjadathick@gmail.com>
2016-05-14 17:54:48 +1000

    added makefile

--- BitQA/drivers/allquestions.cpp
+++ BitQA/drivers/allquestions.cpp
@@ -15,6 +15,31 @@ using namespace std;
 
 int main(){
 	BitQA::HTML::displayHeader();
+	
+	Cgicc cgicc;
+	
+	try {
+		
+		
+		
+	} catch (sql::SQLException &e) {
+		
+		string error(e.what());
+		
+		BitQA::HTML::error("There was an error viewing your question",
+						   "Please go <a href=\"/\">home</a><p>Details: " + error + "</p>"
+						   );
+		
+	} catch (exception &e) {
+		
+		string error(e.what());
+		
+		BitQA::HTML::error("There was an error viewing your question",
+						   "Please go <a href=\"/\">home</a><p>Details: " + error + "</p>"
+						   );
+		
+	}
+	
 	BitQA::HTML::displayFooter();
 	return 0;
 }
