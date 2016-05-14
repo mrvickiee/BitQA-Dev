87ead25334d6914dd0aa26b00d413f94b2c45051
James Marino <jamesmarinoau@gmail.com>
2016-05-14 21:55:22 +1000

    Added RM for build dir

--- api/webhooks/commit/index.php
+++ api/webhooks/commit/index.php
@@ -1,6 +1,7 @@
 <?php
 	
 	$commands = array(
+			"sudo -Hu www-data rm -r /var/www/csci222.com/www/* 2>&1",
 			"sudo -Hu www-data git -C /var/www/csci222.com/ reset --hard HEAD 2>&1",
 			"sudo -Hu www-data git -C /var/www/csci222.com/ pull git@github.com:asjadathick/BitQA-Dev.git 2>&1",
 			"make -C /var/www/csci222.com/BitQA build 2>&1"
