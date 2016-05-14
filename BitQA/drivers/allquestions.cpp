b2e220151151673d36c0139791d33d722455b96f
Asjad <asjadathick@gmail.com>
2016-05-14 17:47:54 +1000

    Stable - New target for allquestions

---
+++ BitQA/drivers/allquestions.cpp
@@ -0,0 +1,20 @@
+//
+//  allquestions.cpp
+//  BitQA
+//
+//  Created by Asjad Athick on 14/05/2016.
+//  Copyright © 2016 CSCI222. All rights reserved.
+//
+
+#include <stdio.h>
+#include "../includes/database.hpp"
+#include "../includes/html.hpp"
+
+using namespace cgicc;
+using namespace std;
+
+int main(){
+	BitQA::HTML::displayHeader();
+	BitQA::HTML::displayFooter();
+	return 0;
+}
