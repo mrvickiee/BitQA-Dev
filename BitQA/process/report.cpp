42144df17eb4942c42d5051d00caa888aae4c917
mt156 <mt156@uowmail.edu.au>
2016-05-15 19:14:35 +1000

    Updated report class functionality
    All functions attempted excluding activity diagram, top 5 posted questions/answers; not all work though for various reasons

--- BitQA/process/report.cpp
+++ BitQA/process/report.cpp
@@ -5,27 +5,87 @@ report functionality declaration
 */
 #include "report.hpp"
 
-Report::Report(){}
+Report::Report(){
+	this->userid = "";
+}
+
+Report::Report(string userId){
+	this->userid = userId;
+}
+
+void Report::setUserId(string userId){
+	this->userid = userId;
+}
+
 
 //Homepage reports
 
 void Report::recQuestions(){
-	cout << "<p>recQuestions</p>";
-
 	/*
-	SELECT questiontitle, dateposted
-	FROM tblQuestion?
-	WHERE [the tags this user has] = [tags the current question has]
-	ORDER BY dateposted DESC LIMIT 10;
+	This becomes searchTerm
+	(SELECT tags
+	FROM tblUserTags
+	WHERE id = 'this->userid';)
+
+	SELECT Q.questionTitle, C.utimestamp
+	FROM tblQuestion Q JOIN tblContent C
+	WHERE Q.tags LIKE 'searchTerm'
+	ORDER BY C.utimestamp DESC LIMIT 10;
 	*/
+
+	try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::Statement *stmt;
+		sql::ResultSet *res;
+		sql::ResultSet *subRes;
+		string searchTerm;
+		
+		driver = get_driver_instance();
+		con = driver->connect(BitQA::Database::HOST,
+							  BitQA::Database::USERNAME,
+							  BitQA::Database::PASSWORD
+							  );
+		
+		con->setSchema(BitQA::Database::SCHEMA);
+		
+		stmt = con->createStatement();
+
+		subRes = stmt->executeQuery("SELECT tags FROM tblUserTags WHERE id = '" + this->userid + "'");
+
+		while(res->next()){
+			searchTerm = subRes->getString("tags");
+		}
+
+		res = stmt->executeQuery("SELECT Q.questionTitle, C.utimestamp FROM tblQuestion Q JOIN tblContent C	WHERE Q.tags LIKE '" + searchTerm + "' ORDER BY C.utimestamp DESC LIMIT 10");
+
+		cout << "<style>table, th, td{border: 2px solid black;}</style>";
+		cout << "<table>";
+		cout << "<tr>";
+		cout << "<th>Question Title</th>";
+		cout << "<th>Date posted</th>";
+		cout << "</tr>";
+		while(res->next()){
+			cout << "<tr>";
+			cout << "<td>" << res->getString("questionTitle") << "</td>";
+			cout << "<td>" << res->getString("utimestamp") << "</td>";
+			cout << "</tr>";
+		}
+		cout << "</table>";
+
+		delete res;
+		delete stmt;
+		delete con;
+
+	} catch (sql::SQLException &e) {
+		cout << "<p>Error</p>";
+	}
 }
 
 void Report::topUsers(){
-	cout << "<p>topUsers</p>";
-
 	/*
 	SELECT U.username, R.reputation, U.creationdate
-	FROM tblUser U LEFT OUTER JOIN tblUserReputation R
+	FROM tblUser U JOIN tblUserReputation R
 	WHERE U.id = R.id
 	ORDER BY R.reputation DESC LIMIT 10;
 	*/
@@ -48,20 +108,37 @@ void Report::topUsers(){
 		stmt = con->createStatement();
 		
 		
-		res = stmt->executeQuery("SELECT U.username, R.reputation, U.creationdate FROM tblUser U LEFT OUTER JOIN tblUserReputation R WHERE U.id = R.id ORDER BY R.reputation DESC LIMIT 10");
+		res = stmt->executeQuery("SELECT U.username, R.reputation, U.creationdate FROM tblUser U JOIN tblUserReputation R WHERE U.id = R.id ORDER BY R.reputation DESC LIMIT 10");
+
+		cout << "<style>table, th, td{border: 2px solid black;}</style>";
+		cout << "<table>";
+		cout << "<tr>";
+		cout << "<th>Username</th>";
+		cout << "<th>Reputation</th>";
+		cout << "<th>Creation Date</th>";
+		cout << "</tr>";
+		while(res->next()){
+			cout << "<tr>";
+			cout << "<td>" << res->getString("username") << "</td>";
+			cout << "<td>" << res->getString("reputation") << "</td>";
+			cout << "<td>" << res->getString("creationdate") << "</td>";
+			cout << "</tr>";
+		}
+		cout << "</table>";
+
+		delete res;
+		delete stmt;
+		delete con;
 
 	} catch (sql::SQLException &e) {
 		cout << "<p>Error</p>";
 	}
-
 }
 
 void Report::featured(){
-	cout << "<p>featured</p>";
-
 	/*
 	SELECT F.questionTitle, U.username
-	FROM tblFeatured F LEFT OUTER JOIN tblUser U
+	FROM tblFeatured F JOIN tblUser U
 	WHERE F.qOwner = U.id
 	ORDER BY dateFeatured DESC LIMIT 10;
 	*/
@@ -84,7 +161,25 @@ void Report::featured(){
 		stmt = con->createStatement();
 		
 		
-		res = stmt->executeQuery("SELECT F.questionTitle, U.username FROM tblFeatured F LEFT OUTER JOIN tblUser U WHERE F.qOwner = U.id ORDER BY dateFeatured DESC LIMIT 10");
+		res = stmt->executeQuery("SELECT F.questionTitle, U.username FROM tblFeatured F JOIN tblUser U WHERE F.qOwner = U.id ORDER BY dateFeatured DESC LIMIT 10");
+
+		cout << "<style>table, th, td{border: 2px solid black;}</style>";
+		cout << "<table>";
+		cout << "<tr>";
+		cout << "<th>Question Title</th>";
+		cout << "<th>Username</th>";
+		cout << "</tr>";
+		while(res->next()){
+			cout << "<tr>";
+			cout << "<td>" << res->getString("questionTitle") << "</td>";
+			cout << "<td>" << res->getString("username") << "</td>";
+			cout << "</tr>";
+		}
+		cout << "</table>";
+
+		delete res;
+		delete stmt;
+		delete con;
 
 	} catch (sql::SQLException &e) {
 		cout << "<p>Error</p>";
@@ -92,13 +187,71 @@ void Report::featured(){
 }
 
 void Report::topQuestions(){
-	cout << "<p>topQuestions</p>";
-
 	/*
 	SELECT Q.questionTitle, C.upvotes
-	FROM tblQuestion Q LEFT OUTER JOIN tblContent C
+	FROM tblQuestion Q JOIN tblContent C
 	WHERE Q.contentId = C.id
 	ORDER BY C.upvotes DESC LIMIT 10;
+	*/
+
+	try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::Statement *stmt;
+		sql::ResultSet *res;
+		
+		driver = get_driver_instance();
+		con = driver->connect(BitQA::Database::HOST,
+							  BitQA::Database::USERNAME,
+							  BitQA::Database::PASSWORD
+							  );
+		
+		con->setSchema(BitQA::Database::SCHEMA);
+		
+		stmt = con->createStatement();
+
+
+//		res = stmt->executeQuery("SELECT id, upvotes FROM tblContent ORDER BY upvotes DESC LIMIT 10");
+//		res = stmt->executeQuery("SELECT tblQuestion.questionTitle, tblContent.upvotes FROM tblQuestion LEFT OUTER JOIN tblContent WHERE tblQuestion.contentId = tblContent.id ORDER BY tblContent.upvotes DESC LIMIT 10");
+//		res = stmt->executeQuery("SELECT Q.questionTitle, C.upvotes FROM tblQuestion Q LEFT OUTER JOIN tblContent C WHERE Q.contentId = C.id ORDER BY C.upvotes DESC LIMIT 10");
+		res = stmt->executeQuery("SELECT Q.questionTitle, C.upvotes FROM tblQuestion Q JOIN tblContent C WHERE Q.contentId = C.id ORDER BY C.upvotes DESC LIMIT 10");
+
+		cout << "<style>table, th, td{border: 2px solid black;}</style>";
+		cout << "<table>";
+		cout << "<tr>";
+		cout << "<th>Question Title</th>";
+		cout << "<th>Upvotes</th>";
+		cout << "</tr>";
+		while(res->next()){
+			cout << "<tr>";
+			cout << "<td>" << res->getString("questionTitle") << "</td>";
+			cout << "<td>" << res->getString("upvotes") << "</td>";
+			cout << "</tr>";
+		}
+		cout << "</table>";
+
+		delete res;
+		delete stmt;
+		delete con;
+
+	} catch (sql::SQLException &e) {
+		cout << "<p>Error</p>";
+	}
+
+}
+
+//User profile reports
+void Report::activityGraph(){
+	cout << "<p>activityGraph</p>";
+}
+
+void Report::topTags(){
+	/*
+	SELECT Q.tags, count(*) AS frequency
+	FROM tblQuestion Q JOIN tblContent C
+	WHERE Q.aowner = 'this->userid'
+	GROUP BY Q.tags
+	ORDER BY count(*) DESC LIMIT 5
 	*/
 
 	try {
@@ -119,21 +272,32 @@ void Report::topQuestions(){
 		stmt = con->createStatement();
 		
 		
-		res = stmt->executeQuery("SELECT Q.questionTitle, C.upvotes FROM tblQuestion Q LEFT OUTER JOIN tblContent C WHERE Q.contentId = C.id ORDER BY C.upvotes DESC LIMIT 10");
+//		res = stmt->executeQuery("SELECT Q.tags, count(*) AS frequency FROM tblQuestion Q JOIN tblContent C WHERE Q.aowner = '" + this->userid + "' GROUP BY Q.tags ORDER BY count(*) DESC LIMIT 5");
+//		res = stmt->executeQuery("SELECT Q.tags, Q.count(Q.tags) AS frequency FROM tblQuestion Q JOIN tblContent C WHERE Q.aowner = '" + this->userid + "' GROUP BY Q.tags ORDER BY Q.count(Q.tags) DESC LIMIT 5");
+		res = stmt->executeQuery("SELECT tags, count(*) AS frequency FROM tblQuestion WHERE qowner = '" + this->userid + "' GROUP BY tags ORDER BY count(*) DESC LIMIT 5");
+
+		cout << "<style>table, th, td{border: 2px solid black;}</style>";
+		cout << "<table>";
+		cout << "<tr>";
+		cout << "<th>Tags</th>";
+		cout << "<th>Frequency</th>";
+		cout << "</tr>";
+		while(res->next()){
+			cout << "<tr>";
+			cout << "<td>" << res->getString("tags") << "</td>";
+			cout << "<td>" << res->getString("frequency") << "</td>";
+			cout << "</tr>";
+		}
+		cout << "</table>";
+
+		delete res;
+		delete stmt;
+		delete con;
 
 	} catch (sql::SQLException &e) {
 		cout << "<p>Error</p>";
 	}
 
-}
-
-//User profile reports
-void Report::activityGraph(){
-	cout << "<p>activityGraph</p>";
-}
-
-void Report::topTags(){
-	cout << "<p>topTags</p>";
 }
 
 void Report::topPostedQuestions(){
@@ -145,6 +309,54 @@ void Report::topPostedAnswers(){
 }
 
 void Report::postHistory(){
-	cout << "<p>postHistory</p>";
-}
+	/*
+	"SELECT Con.id, Con.utimestamp
+	FROM tblContent Con, tblAnswer A, tblQuestion Q, tblComment C
+	WHERE (A.aowner = '" + this->userid + "') OR (Q.qowner = '" + this->userid + "') OR (C.aowner = '" + this->userid + "')
+	ORDER BY utimestamp DESC LIMIT 10"
+	*/
 
+	try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::Statement *stmt;
+		sql::ResultSet *res;
+		
+		
+		driver = get_driver_instance();
+		con = driver->connect(BitQA::Database::HOST,
+							  BitQA::Database::USERNAME,
+							  BitQA::Database::PASSWORD
+							  );
+		
+		con->setSchema(BitQA::Database::SCHEMA);
+		
+		stmt = con->createStatement();
+		
+		
+//		res = stmt->executeQuery("SELECT Con.id, Con.utimestamp	FROM (tblContent Con, tblAnswer A, tblQuestion Q, tblComment C WHERE (A.aowner = '" + this->userid + "') OR (Q.qowner = '" + this->userid + "') OR (C.aowner = '" + this->userid + "') ORDER BY Con.utimestamp DESC LIMIT 10");
+		res = stmt->executeQuery("SELECT Con.id, Con.utimestamp	FROM (tblContent Con JOIN tblAnswer A) JOIN (tblContent Con JOIN tblQuestion Q) WHERE (A.aowner = '" + this->userid + "') OR (Q.qowner = '" + this->userid + "') ORDER BY Con.utimestamp DESC LIMIT 10");
+
+		cout << "<style>table, th, td{border: 2px solid black;}</style>";
+		cout << "<table>";
+		cout << "<tr>";
+		cout << "<th>Content ID</th>";
+		cout << "<th>Timestamp</th>";
+		cout << "</tr>";
+		while(res->next()){
+			cout << "<tr>";
+			cout << "<td>" << res->getString("id") << "</td>";
+			cout << "<td>" << res->getString("utimestamp") << "</td>";
+			cout << "</tr>";
+		}
+		cout << "</table>";
+
+		delete res;
+		delete stmt;
+		delete con;
+
+	} catch (sql::SQLException &e) {
+		cout << "<p>Error</p>";
+	}
+
+}
