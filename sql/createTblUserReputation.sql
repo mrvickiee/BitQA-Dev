81809e84caf0dc7948d6aa44520896932a0d747d
mt156 <mt156@uowmail.edu.au>
2016-05-15 19:09:00 +1000

    Uploaded new tables
    Needed for the report subsystem

---
+++ sql/createTblUserReputation.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `tblUserReputation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reputation` int() DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ sql/tblUserTags.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `tblUserTags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tags` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
