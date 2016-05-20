a7a12b284304a4e486e0a584797ab02746928f9a
Asjad <asjadathick@gmail.com>
2016-05-20 12:26:31 +1000

    Working - view all questions with page partitioning

--- BitQA/drivers/allquestions.cpp
+++ BitQA/drivers/allquestions.cpp
@@ -19,14 +19,52 @@ int main(){
 	Cgicc cgicc;
 	
 	try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::PreparedStatement *prep_stmt;
+		sql::ResultSet *res;
 		
+		driver = get_driver_instance();
+		con = driver->connect(BitQA::Database::HOST,
+							  BitQA::Database::USERNAME,
+							  BitQA::Database::PASSWORD
+							  );
+		
+		con->setSchema(BitQA::Database::SCHEMA);
+		
+		int fromVal, toVal;
+		if (cgicc("page") == "")
+			fromVal = 0;
+		else
+			fromVal = ((stoi(cgicc("page")) -1) * 100);
+		
+		
+		if (fromVal < 0) {
+			throw string("Page out of range");
+		}
+		
+		toVal = fromVal + 100;
+		
+		prep_stmt = con->prepareStatement("CALL GetAllQuestions(?,?);");
+		prep_stmt->setInt(1, fromVal);
+		prep_stmt->setInt(2, toVal);
+		res = prep_stmt->executeQuery();
+		
+		//print results
+		while (res->next()) {
+			cout << res->getString("questionTitle") << endl;
+		}
+		
+		
+		delete prep_stmt;
+		delete con;
 		
 		
 	} catch (sql::SQLException &e) {
 		
 		string error(e.what());
 		
-		BitQA::HTML::error("There was an error viewing your question",
+		BitQA::HTML::error("There was an error with SQL",
 						   "Please go <a href=\"/\">home</a><p>Details: " + error + "</p>"
 						   );
 		
@@ -34,10 +72,14 @@ int main(){
 		
 		string error(e.what());
 		
-		BitQA::HTML::error("There was an error viewing your question",
+		BitQA::HTML::error("There was an error viewing questions",
 						   "Please go <a href=\"/\">home</a><p>Details: " + error + "</p>"
 						   );
 		
+	} catch (string e){
+		BitQA::HTML::error("There was an error viewing questions",
+						   "Please go <a href=\"/\">home</a><p></p>"
+						   );
 	}
 	
 	BitQA::HTML::displayFooter();
