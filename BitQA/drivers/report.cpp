33a5aebb58db569a60386a7edbf32ec82e63c7ac
James Marino <jamesmarinoau@gmail.com>
2016-05-27 02:36:25 +1000

    Major fixes of multiple issues in reporting

--- BitQA/drivers/report.cpp
+++ BitQA/drivers/report.cpp
@@ -12,32 +12,90 @@ using namespace std;
 
 int main()
 {
-	Report report("0");
-	Cgicc cgicc;
 	
-    BitQA::HTML::displayHeader("Reports", cgicc);
+	try {
+		
+		Cgicc cgicc;
+		BitQA::HTML::displayHeader("Reports", cgicc);
+		
+		CgiEnvironment environment = cgicc.getEnvironment();
+		const_cookie_iterator cci;
+		
+		string userName = "";
+		string displayName = "";
+		
+		for (cci = environment.getCookieList().begin();
+			 cci != environment.getCookieList().end();
+			 cci++) {
+			if (cci->getName() == "username") {
+				userName = cci->getValue();
+			} else if (cci->getName() == "displayname") {
+				displayName = cci->getValue();
+			}
+		}
+		
+		Report report;
+		
+		if (userName.length() > 0) {
+			report.setUserId(userName);
+		}
+		
+		cout << "<h1>Reporting</h1>";
+		
+		cout << "<br><hr><br>";
+		cout << "<h3>Recommended Questions</h3><br>";
+		report.recQuestions();
+		 
+		cout << "<br><hr><br>";
+		cout << "<h3>Top 10 Users</h3><br>";
+		report.topUsers();
+		 
+		cout << "<br><hr><br>";
+		cout << "<h3>Recent Featured Questions</h3><br>";
+		report.featured();
 
-	cout << "<h1>Homepage Reports</h1>";
-	cout << "<h3>Reccomended Questions<br></h3>";
-	//report.recQuestions();
-	cout << "<h3>Top 10 Users<br></h3>";
-	report.topUsers();
-	cout << "<h3>Recent Featured Questions<br></h3>";
-	report.featured();
-	cout << "<h3>Top 10 Most Upvoted Questions<br></h3>";
-	report.topQuestions();
-
-	cout << "<h1><br>User Profile Reports<h1>";
-	cout << "<h3>Activity Graph<br></h3>";
-	report.activityGraph();
-	cout << "<h3>Top 5 Tags<br></h3>";
-	report.topTags();
-	cout << "<h3>Top 5 Posted Answers<br></h3>";
-	report.topPostedAnswers();
-	cout << "<h3>Top 5 Posted Questions<br></h3>";
-	report.topPostedQuestions();
-	cout << "<h3>Posting History<br></h3>";
-	report.postHistory();
+		cout << "<br><hr><br>";
+		cout << "<h3>Top 10 Most Upvoted Questions</h3><br>";
+		report.topQuestions();
+		
+		cout << "<br><hr><h1>User Profile Reports<h1>";
+		
+		cout << "<br>";
+		cout << "<h3>Activity Graph</h3><br>";
+		report.activityGraph();
+		
+		cout << "<br><hr><br>";
+		cout << "<h3>Top 5 Tags</h3><br>";
+		report.topTags();
+		
+		cout << "<br><hr><br>";
+		cout << "<h3>Posting History</h3><br>";
+		report.postHistory();
+		
+		/*
+		 cout << "<h3>Top 5 Posted Answers<br></h3>";
+		 report.topPostedAnswers();
+		 cout << "<h3>Top 5 Posted Questions<br></h3>";
+		 report.topPostedQuestions();
+		 */
+		
+	} catch (sql::SQLException &e) {
+		
+		string error(e.what());
+		
+		BitQA::HTML::error("There was an error generating the report",
+						   "Please go <a href=\"/\">home</a><p>Details: " + error + "</p>"
+						   );
+		
+	} catch (exception &e) {
+		
+		string error(e.what());
+		
+		BitQA::HTML::error("There was an error generating the report",
+						   "Please go <a href=\"/\">home</a><p>Details: " + error + "</p>"
+						   );
+		
+	}
 
     BitQA::HTML::displayFooter();
     
