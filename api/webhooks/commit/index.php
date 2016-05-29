053dc04df45d3894d1d6a47fcfe9e0bdec031fc9
James Marino <jamesmarinoau@gmail.com>
2016-05-29 13:16:07 +1000

    Updated auto build

--- api/webhooks/commit/index.php
+++ api/webhooks/commit/index.php
@@ -2,6 +2,7 @@
 	
 	$commands = array(
 			"sudo -Hu www-data rm -r /var/www/csci222.com/www/* 2>&1",
+			"sudo -Hu www-data mkdir /var/www/csci222.com/www/api 2>&1",
 			"sudo -Hu www-data git -C /var/www/csci222.com/ reset --hard HEAD 2>&1",
 			"sudo -Hu www-data git -C /var/www/csci222.com/ pull git@github.com:asjadathick/BitQA-Dev.git 2>&1",
 			"sudo -Hu www-data make -C /var/www/csci222.com/BitQA build 2>&1"
